526c04eeb975fcae9067ccf8e4f4f2a421c43188
honeyyadav <mr.honeyyadav@gmail.com>
2024-11-27 22:44:34 +0530

    sync

--- flake.nix
+++ flake.nix
@@ -5,16 +5,50 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
   let
     configuration = { pkgs, ... }: {
+
+      nixpkgs.config.allowUnfree = true;
+
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.vim
+        [ 
+          pkgs.vim
+          pkgs.fzf
+          pkgs.k9s
+          pkgs.kubectl
+          pkgs.kubectx
+          pkgs.lens
         ];
+
+      homebrew = {
+        enable = true;
+        casks = [ 
+          # "firefox"
+        ];
+        onActivation.cleanup = "zap";
+        onActivation.autoUpdate = true;
+        onActivation.upgrade = true;
+      };
+
+      # TODO:-
+      # tailscale or twingate
+      
+
+      system.defaults = {
+        dock.autohide = true;
+        dock.orientation = "left";
+        dock.persistent-apps = [
+          "/Applications/Google Chrome.app"
+        ];
+        finder.FXPreferredViewStyle = "clmv";
+        loginwindow.GuestEnabled = false;
+      };
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
@@ -37,7 +71,36 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#macpersonal
     darwinConfigurations."macpersonal" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            enableRosetta = true;
+
+            # User owning the Homebrew prefix
+            user = "honeyyadav";
+
+            autoMigrate = true;
+
+            # Optional: Declarative tap management
+            # taps = {
+            #   "homebrew/homebrew-core" = homebrew-core;
+            #   "homebrew/homebrew-cask" = homebrew-cask;
+            #   "homebrew/homebrew-bundle" = homebrew-bundle;
+            # };
+
+            # Optional: Enable fully-declarative tap management
+            #
+            # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+            # mutableTaps = false;
+          };
+        }
+      ];
     };
   };
 }
